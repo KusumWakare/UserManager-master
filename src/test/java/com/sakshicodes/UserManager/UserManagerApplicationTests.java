ff8e1daa908dd3ed7cbbf9d0ed151416cc110880
KusumWakare <kusumwakare511@gmail.com>
2023-06-03 12:35:13 +0530

    code commit

---
+++ src/test/java/com/sakshicodes/UserManager/UserManagerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sakshicodes.UserManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
